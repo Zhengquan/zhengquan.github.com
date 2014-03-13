f1d0523f61ddc70be66e4f256df82d00e507bf13
Zhengquan Yang <yangzhengquan@gmail.com>
2014-03-14 00:42:56 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+mkdir -p /tmp/blog 
+rm -rf /tmp/blog/*
+
+# generate static files
+bundle exec rake generate
+mv _site/* /tmp/blog
+
+git checkout master
+rm -rf *
+mv /tmp/blog/* .
+
+message="Site updated at $(date +'%d-%m-%Y')"
+
+git add -A
+git commit -am $message
+
